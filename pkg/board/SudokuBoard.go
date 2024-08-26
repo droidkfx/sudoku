c228f9ee3da53c67df073f1dda608e2bebd3f168
droidkfx <kam157190@gmail.com>
2024-08-26 17:22:07 -0500

    Create board with numbers method to create sudoku boards

--- pkg/board/SudokuBoard.go
+++ pkg/board/SudokuBoard.go
@@ -4,6 +4,16 @@ type SudokuBoard struct {
 	board [9][9]int
 }
 
+func BoardFromNumbers(data [9][9]int) *SudokuBoard {
+	board := [9][9]int{}
+	for i, list := range data {
+		for j, num := range list {
+			board[i][j] = num
+		}
+	}
+	return &SudokuBoard{board: board}
+}
+
 func (s *SudokuBoard) GetAt(x, y int) int {
 	return s.board[y][x]
 }

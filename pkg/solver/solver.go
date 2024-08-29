c0932d3c55a4e4ee11132e3ddf79773bb447ea49
droidkfx <kam157190@gmail.com>
2024-08-28 22:02:58 -0500

    Abstract guess config

--- pkg/solver/solver.go
+++ pkg/solver/solver.go
@@ -2,6 +2,7 @@ package solver
 
 import (
 	"fmt"
+	"math/rand/v2"
 
 	"droidkfx.com/sudoku/pkg/board"
 )
@@ -12,12 +13,42 @@ type SolveMetrics struct {
 }
 
 type GuessSolverConfig struct {
-	NumberOrder []int
+	NumberOrder GuessOrderProvider
+}
+
+type GuessOrderProvider func(i, j, v int) int
+
+func NewStaticOrderGuesser(order []int) GuessOrderProvider {
+	return func(i, j, v int) int {
+		return order[v]
+	}
+}
+
+func NewRandomOrderGuesser(swapIteration int) GuessOrderProvider {
+	list := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	listI := 0
+	return func(_, _, v int) int {
+		listI++
+		if listI >= 9 {
+			for i := 0; i < swapIteration; i++ {
+				x, y := rand.IntN(9), rand.IntN(9)
+				list[x], list[y] = list[y], list[x]
+			}
+		}
+
+		return list[v]
+	}
 }
 
 func DefaultGuessConfig() GuessSolverConfig {
 	return GuessSolverConfig{
-		NumberOrder: []int{0, 1, 2, 3, 4, 5, 6, 7, 8},
+		NumberOrder: NewStaticOrderGuesser([]int{0, 1, 2, 3, 4, 5, 6, 7, 8}),
+	}
+}
+
+func GuessConfig(provider GuessOrderProvider) GuessSolverConfig {
+	return GuessSolverConfig{
+		NumberOrder: provider,
 	}
 }
 
@@ -36,10 +67,11 @@ func solveByGuessing(cfg GuessSolverConfig, board *board.SudokuBoard, values [9]
 	metrics *SolveMetrics) bool {
 	if board.GetAt(x, y) == 0 {
 		anyPossibleValues := false
-		for _, i := range cfg.NumberOrder {
-			if values[x][y][i] {
+		for v := 0; v < 9; v++ {
+			number := cfg.NumberOrder(x, y, v)
+			if values[x][y][number] {
 				anyPossibleValues = true
-				if tryValue(cfg, board, values, x, y, i+1, metrics) {
+				if tryValue(cfg, board, values, x, y, number+1, metrics) {
 					return true
 				} else {
 					board.SetAt(x, y, 0)

020094829d1f77692036142aa6e869d802a59772
droidkfx <kam157190@gmail.com>
2024-09-16 10:36:11 -0500

    add options to loader

--- cmd/loadSelected/main.go
+++ cmd/loadSelected/main.go
@@ -8,10 +8,12 @@ import (
 )
 
 func main() {
-	r, sd := repository.NewSudokuBoardRepo("./data/nyt/med")
+	// r, sd := repository.NewSudokuBoardRepo("./data/nyt/med")
+	// r, sd := repository.NewSudokuBoardRepo("./data/nyt/easy")
+	r, sd := repository.NewSudokuBoardRepo("./data")
 	defer sd()
 
-	b := r.GetNumber(0)
+	b := r.GetNumber(50)
 	fmt.Println(b)
 	solution := solver.SolveByStrategies(b)
 	for _, step := range solution {
